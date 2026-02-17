df29d264d6c35d1ca93f593156bdf02e33dff305
pog0sian <pogosian.david2018@yandex.ru>
2026-02-17 15:50:56 +0700

    init

---
+++ src/main/kotlin/com/example/springjpa/SpringJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJpaApplication>(*args)
+}
